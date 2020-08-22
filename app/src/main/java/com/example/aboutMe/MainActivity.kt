8f55014d7c9b3ed4cbb00a9b79165c47a25264d7
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-08-22 16:33:58 +0530

    Nickname Button Listener

--- app/src/main/java/com/example/aboutMe/MainActivity.kt
+++ app/src/main/java/com/example/aboutMe/MainActivity.kt
@@ -10,6 +10,9 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
